5e0a5fbba2596b2779ebe984f691564453a57f72
Ville M. Vainio <ville.vainio@basware.com>
2022-01-08 21:27:28 +0200

    add test, setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pkska',
+      version='1.0.0',
+      description='Desciption for pkska here',
+      author='Ville M. Vainio',
+      author_email='ville.vainio@basware.com',
+      url='https://github.com/vivainio/pkska',
+      packages=['pkska'],
+      install_requires=[],
+     )
